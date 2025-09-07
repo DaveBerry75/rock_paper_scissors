c93c213c0c4c1968927adafe0f5b10cfcefe8be1
Dave Berry <218662951+DaveBerry75@users.noreply.github.com>
2025-09-07 19:24:30 +0100

    Exchange number for string value

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 // Create a function called getComputerChoice and use math.random() and Math.floor to get a random integer from 1 - 3
 function getComputerChoice() {
-    console.log(Math.floor(Math.random() * 3) + 1);
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+// Assign rock, paper and scissors to 1, 2 and 3 respectively
+    if (randomNum === 1) {
+        console.log("rock");
+    } else if (randomNum === 2) {
+        console.log("paper");
+    } else {
+        console.log("scissors");
+    }
 }
 
 getComputerChoice();
 
-// Use only numbers 1, 2 and 3
 
-// Assign rock, paper and scissors to 1, 2 and 3 respectively
 
-//  Test to make sure code returns rock, paper or scissors randomly
+
+
