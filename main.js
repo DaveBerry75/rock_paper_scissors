dfc5cb85c53a969fb7a1440abc9e4757a7b919dd
Dave Berry <218662951+DaveBerry75@users.noreply.github.com>
2025-09-07 17:05:04 +0100

    Create a function which returns a random number

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-// Create a function called getComputerChoice
+// Create a function called getComputerChoice and use math.random() to get a random number between 0 and 1
+function getComputerChoice() {
+    console.log(Math.random());
+}
 
-// Use math.random() to get a random number between 0 and 1
-
+getComputerChoice();
 // Multiply these numbers by ten and round down to the nearest integer
 
 // Use only numbers 1, 2 and 3
