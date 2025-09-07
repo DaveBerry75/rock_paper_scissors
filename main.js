1e0ef0fa91c0d57b8c0348c0c76c50e45ae9e2fa
Dave Berry <218662951+DaveBerry75@users.noreply.github.com>
2025-09-07 16:54:49 +0100

    Add comments describing code for computers choice

---
+++ main.js
@@ -0,0 +1,11 @@
+// Create a function called getComputerChoice
+
+// Use math.random() to get a random number between 0 and 1
+
+// Multiply these numbers by ten and round down to the nearest integer
+
+// Use only numbers 1, 2 and 3
+
+// Assign rock, paper and scissors to 1, 2 and 3 respectively
+
+//  Test to make sure code returns rock, paper or scissors randomly
